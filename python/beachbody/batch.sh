4a523f11b390f5bcc27374511ff551cc83715b09
Pablo Melchi <pablo.melchi@dummy.com>
2024-05-15 06:15:25 -0500

    How to run scripts

--- python/beachbody/batch.sh
+++ python/beachbody/batch.sh
@@ -2,4 +2,16 @@
 
 cd /srv/dev-disk-by-label-HDDStorage/OMV-Downloader/downloader
 source venv/bin/activate
-python download-bodi.py ./liifmore/liif-more.json ./liifmore/ >> /var/log/beachbody.log
+
+
+#Regular programs
+# python download-bodi.py ./5er/5er.json ./5er/ > /var/log/beachbody.log
+# python download-bodi.py ./the-20s/the-20s.json ./the-20s/ >> /var/log/beachbody.log
+# python download-bodi.py ./chopwood-carrywater/chopwood-carrywater.json ./chopwood-carrywater/ >> /var/log/beachbody.log
+
+#Super-block
+python download-superblock.py ./superblock-liifmore.json ./superblock-liifmore > /var/log/beachbody.log
+
+
+# Archive 
+#python download-bodi.py ./dig-deeper/dig-deeper.json ./dig-deeper/ >> /var/log/beachbody.log
